34cf14173202da7386b37d33bf2c8cc0c2908f2d
Johannes Hanek <73287080+johanneshanek@users.noreply.github.com>
2022-12-27 12:08:48 +0100

    Initial commit

---
+++ usbtemp/main.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.CheckBox8 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox7 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ZedGraphControl1 = New ZedGraph.ZedGraphControl()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox8)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox7)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox6)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox5)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox4)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.ToolStrip1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.StatusStrip1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.ZedGraphControl1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(800, 450)
+        Me.SplitContainer1.SplitterDistance = 266
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'CheckBox8
+        '
+        Me.CheckBox8.Checked = True
+        Me.CheckBox8.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox8.Dock = System.Windows.Forms.DockStyle.Top
+        Me.CheckBox8.Location = New System.Drawing.Point(0, 207)
+        Me.CheckBox8.Name = "CheckBox8"
+        Me.CheckBox8.Size = New System.Drawing.Size(266, 26)
+        Me.CheckBox8.TabIndex = 8
+        Me.CheckBox8.Text = "Channel 8"
+        Me.CheckBox8.UseVisualStyleBackColor = True
+        '
+        'CheckBox7
+        '
+        Me.CheckBox7.Checked = True
+        Me.CheckBox7.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox7.Dock = System.Windows.Forms.DockStyle.Top
+        Me.CheckBox7.Location = New System.Drawing.Point(0, 181)
+        Me.CheckBox7.Name = "CheckBox7"
+        Me.CheckBox7.Size = New System.Drawing.Size(266, 26)
+        Me.CheckBox7.TabIndex = 7
+        Me.CheckBox7.Text = "Channel 7"
+        Me.CheckBox7.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.Checked = True
+        Me.CheckBox6.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox6.Dock = System.Windows.Forms.DockStyle.Top
+        Me.CheckBox6.Location = New System.Drawing.Point(0, 155)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(266, 26)
+        Me.CheckBox6.TabIndex = 6
+        Me.CheckBox6.Text = "Channel 6"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.Checked = True
+        Me.CheckBox5.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox5.Dock = System.Windows.Forms.DockStyle.Top
+        Me.CheckBox5.Location = New System.Drawing.Point(0, 129)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(266, 26)
+        Me.CheckBox5.TabIndex = 5
+        Me.CheckBox5.Text = "Channel 5"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.Checked = True
+        Me.CheckBox4.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.CheckBox4.Location = New System.Drawing.Point(0, 103)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(266, 26)
+        Me.CheckBox4.TabIndex = 4
+        Me.CheckBox4.Text = "Channel 4"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.Checked = True
+        Me.CheckBox3.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.CheckBox3.Location = New System.Drawing.Point(0, 77)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(266, 26)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "Channel 3"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.CheckBox2.Location = New System.Drawing.Point(0, 51)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(266, 26)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Channel 2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.CheckBox1.Location = New System.Drawing.Point(0, 25)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(266, 26)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Channel 1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripButton2, Me.ToolStripButton3, Me.ToolStripSeparator1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(266, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = Global.usbtemp.My.Resources.Resources.icons8_anfang_50
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton1.Text = "Start"
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton2.Image = Global.usbtemp.My.Resources.Resources.icons8_stop_50
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton2.Text = "Stop"
+        '
+        'ToolStripButton3
+        '
+        Me.ToolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton3.Image = Global.usbtemp.My.Resources.Resources.icons8_symbol__löschen_24
+        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton3.Name = "ToolStripButton3"
+        Me.ToolStripButton3.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton3.Text = "Alles löschen"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ZedGraphControl1
+        '
+        Me.ZedGraphControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ZedGraphControl1.Location = New System.Drawing.Point(0, 0)
+        Me.ZedGraphControl1.Name = "ZedGraphControl1"
+        Me.ZedGraphControl1.ScrollGrace = 0R
+        Me.ZedGraphControl1.ScrollMaxX = 0R
+        Me.ZedGraphControl1.ScrollMaxY = 0R
+        Me.ZedGraphControl1.ScrollMaxY2 = 0R
+        Me.ZedGraphControl1.ScrollMinX = 0R
+        Me.ZedGraphControl1.ScrollMinY = 0R
+        Me.ZedGraphControl1.ScrollMinY2 = 0R
+        Me.ZedGraphControl1.Size = New System.Drawing.Size(530, 450)
+        Me.ZedGraphControl1.TabIndex = 0
+        Me.ZedGraphControl1.UseExtendedPrintDialog = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(266, 22)
+        Me.StatusStrip1.TabIndex = 9
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents ZedGraphControl1 As ZedGraph.ZedGraphControl
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents CheckBox8 As CheckBox
+    Friend WithEvents CheckBox7 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents ToolStripButton3 As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents StatusStrip1 As StatusStrip
+End Class
---
+++ usbtemp/main.vb
@@ -0,0 +1,141 @@
+﻿Public Class Form1
+
+    Public Device As New USBDevice
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Text = Device.Name
+        Initialize()
+    End Sub
+
+    Private Sub Initialize()
+        ZedGraphControl1.GraphPane.CurveList.Clear()
+
+        For i As Integer = 0 To 7
+            ZedGraphControl1.GraphPane.CurveList.Add(New ZedGraph.LineItem("Channel " & i + 1) With
+                                                     {.Color = GetColor(i),
+                                                     .Symbol = New ZedGraph.Symbol(ZedGraph.SymbolType.None, GetColor(i))})
+            If Not Device.Channel(i).Connected Then
+                Dim cb As CheckBox = Controls.Find("CheckBox" & i + 1, True).First
+                cb.Checked = False
+                cb.BackColor = Color.LightGray
+            End If
+        Next
+
+        ZedGraphControl1.GraphPane.XAxis.Type = ZedGraph.AxisType.Date
+        ZedGraphControl1.GraphPane.Title.Text = "USB temp Data Plot"
+    End Sub
+
+    Private Function GetColor(index As Integer) As Color
+        Select Case index
+            Case 0
+                Return Color.Black
+            Case 1
+                Return Color.Red
+            Case 2
+                Return Color.Blue
+            Case 3
+                Return Color.Green
+            Case 4
+                Return Color.Brown
+            Case 5
+                Return Color.Pink
+            Case 6
+                Return Color.Orange
+            Case 7
+                Return Color.LightBlue
+            Case 8
+                Return Color.LightGreen
+        End Select
+    End Function
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        For i As Integer = 0 To 7
+            Dim cb As CheckBox = Controls.Find("CheckBox" & i + 1, True).First
+            If cb.Checked Then
+                Dim data As datapoint = Device.Measure(i)
+                ZedGraphControl1.GraphPane.CurveList(i).AddPoint(data.MeasurementDate.ToOADate, data.Value)
+                ZedGraphControl1.AxisChange()
+                ZedGraphControl1.Refresh()
+                cb.Text = "Channel " & i + 1 & " [Last = " & data.Value & " °C]"
+            End If
+        Next
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+        Timer1.Stop()
+        Device.Clear()
+        For i As Integer = 0 To 7
+            ZedGraphControl1.GraphPane.CurveList(i).Clear()
+            Dim cb As CheckBox = Controls.Find("CheckBox" & i + 1, True).First
+            cb.Text = "Channel " & i + 1
+        Next i
+        ZedGraphControl1.Refresh()
+    End Sub
+
+End Class
+
+Public Class USBDevice
+    Private DaqBoard As New MccDaq.MccBoard(0)
+    Public Channel(7) As Channel
+
+    Public ReadOnly Property Name() As String
+        Get
+            Return DaqBoard.BoardName
+        End Get
+    End Property
+
+    Public Sub New()
+        For i As Integer = 0 To 7
+            Channel(i) = New Channel
+            If GetTemperature(i).Value = -9999 Then Channel(i).Connected = False
+        Next
+    End Sub
+
+    Private Function GetTemperature(ChannelIndex As Integer) As datapoint
+        Dim Value As Single
+        DaqBoard.TIn(ChannelIndex, MccDaq.TempScale.Celsius, Value, MccDaq.ThermocoupleOptions.NoFilter)
+        Return New datapoint(Date.Now, Value)
+    End Function
+
+    Public Sub Clear()
+        For i As Integer = 0 To 7
+            Channel(i).data.Clear()
+        Next i
+    End Sub
+
+    Public Function Measure(Index As Integer) As datapoint
+        Dim dp As datapoint = GetTemperature(Index)
+        Channel(Index).data.Add(dp)
+        Return dp
+    End Function
+
+End Class
+
+Public Class Channel
+    Public Connected As Boolean = True
+    Public data As New List(Of datapoint)
+    Public Name As String
+End Class
+
+Public Class datapoint
+    Public MeasurementDate As Date
+    Public Value As Double
+    Public Sub New(Value As Double)
+        MeasurementDate = Date.Now
+        Me.Value = Value
+    End Sub
+
+    Public Sub New(meas_date As Date, value As Double)
+        Me.MeasurementDate = meas_date 'New Date(meas_date.Year, meas_date.Month, meas_date.Day, meas_date.Hour, meas_date.Minute, meas_date.Second)
+        Me.Value = value
+    End Sub
+
+End Class
